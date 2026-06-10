8f04e5dc09ce61f61f5e6124d4d28e386f8c38b4
BartoszBudek <290135@student.pwr.edu.pl>
2026-06-10 16:56:13 +0200

    Update ppolab4.cpp

--- ppolab4.cpp
+++ ppolab4.cpp
@@ -50,30 +50,51 @@ public:
     }
 };
 
-class student:public osoba{
+class student : public osoba {
+public:
+    // Użycie virtual zgodnie z konwencją prowadzącego
     virtual void drukuj(){
-        cout<<"STUDENT:\n Imie: "<<imie<<" Nazwisko: "<<nazwisko<<endl;
+        cout<<"STUDENT:\n Imie: "<<imie<<" Nazwisko: "<<nazwisko<<" Indeks: "<<index<<endl;
     }
 };
 
-class prowadzacy:public osoba{
+// Nowa klasa dziedzicząca po studencie, również z virtual
+class student_zaoczny : public student {
+public:
+    virtual void drukuj(){
+        cout<<"STUDENT ZAOCZNY:\n Imie: "<<imie<<" Nazwisko: "<<nazwisko<<" Indeks: "<<index<<endl;
+    }
+};
+
+class prowadzacy : public osoba {
+public:
     virtual void drukuj(){
         cout<<"PROWADZACY:\n Imie: "<<imie<<" Nazwisko: "<<nazwisko<<endl;
     }
 };
 
 int main(){
-    osoba *tab[3];
+    osoba *tab[4];
 
     osoba os1;
     student st1;
+    student_zaoczny stz1;
     prowadzacy pr1;
+
+    os1.setimie("Jan"); os1.setnazwisko("Kowalski"); os1.setindex(100001);
+    st1.setimie("Anna"); st1.setnazwisko("Nowak"); st1.setindex(222333);
+    stz1.setimie("Piotr"); stz1.setnazwisko("Zielinski"); stz1.setindex(555666);
+    pr1.setimie("Tomasz"); pr1.setnazwisko("Wisniewski");
 
     tab[0]=&os1;
     tab[1]=&st1;
-    tab[2]=&pr1;
+    tab[2]=&stz1;
+    tab[3]=&pr1;
 
-    tab[0]->drukuj();
-    tab[1]->drukuj();
-    tab[2]->drukuj();
+    for(int i = 0; i < 4; i++) {
+        tab[i]->drukuj();
+        cout << "--------------------" << endl;
+    }
+
+    return 0;
 }
